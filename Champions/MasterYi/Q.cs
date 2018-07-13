43524c59e14ad1ac10f8e305b797ba759525c5c9
Peter <simber2@btinternet.com>
2018-07-13 16:58:26 +0100

    Fixing crit state

--- Champions/MasterYi/Q.cs
+++ Champions/MasterYi/Q.cs
@@ -60,7 +60,7 @@ namespace Spells
                     float baseDamage = new[] { 25, 60, 95, 130, 165 }[spell.Level - 1] + owner.GetStats().AttackDamage.Total * 1f + owner.GetStats().AbilityPower.Total * 0.7f;
                     Boolean isCrit = new Random().Next(0, 100) < owner.GetStats().CriticalChance.Total;
                     float damage = isCrit ? baseDamage * 1.6f : baseDamage;
-                    enemyTarget.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
+                    enemyTarget.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, isCrit);
 
                 }
                 else if (ApiFunctionManager.UnitIsMinion(enemyTarget))
@@ -68,7 +68,7 @@ namespace Spells
                     float baseDamage = new[] { 75, 100, 125, 150, 175 }[spell.Level - 1] + owner.GetStats().AttackDamage.Total * 1f + owner.GetStats().AbilityPower.Total * 0.7f;
                     Boolean isCrit = new Random().Next(0, 100) < owner.GetStats().CriticalChance.Total;
                     float damage = isCrit ? baseDamage * 1.6f : baseDamage;
-                    enemyTarget.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
+                    enemyTarget.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, isCrit);
                 }
             }
             
